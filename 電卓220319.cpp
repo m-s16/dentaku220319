0383a3e42baffa8fe56e24dc17573cba0dc5f0c5
m-s16 <noir_o9@yahoo.co.jp>
2022-03-20 13:31:51 +0900

    電卓220319unicode対応

--- 電卓220319.cpp
+++ 電卓220319.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <windowsx.h>
+#include <tchar.h>
 #include <string.h> //memset, strcat, strcpy
 #include <stdio.h> //sprintf
 #include <math.h> //atof
@@ -43,7 +44,7 @@ INT_PTR CALLBACK DialogProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
     case WM_INITDIALOG:
         hEWnd = GetDlgItem(hDlg, IDC_EDIT1);
 
-        Edit_SetText(hEWnd, "0");
+        Edit_SetText(hEWnd, TEXT("0"));
         return TRUE;
     case WM_COMMAND:
         switch (LOWORD(wp)) {
@@ -62,87 +63,91 @@ INT_PTR CALLBACK DialogProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
             return TRUE;
         case IDC_BUTTON12:
 
-            strcat_s(str[mode], sizeof(str[mode]), ".");
+            lstrcat(str[mode],  TEXT("."));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON1:
-            strcat_s(str[mode], sizeof(str[mode]), "1");
+            lstrcat(str[mode], TEXT("1"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON2:
-            strcat_s(str[mode], sizeof(str[mode]), "2");
+            lstrcat(str[mode], TEXT("2"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON3:
-            strcat_s(str[mode], sizeof(str[mode]), "3");
+            lstrcat(str[mode], TEXT("3"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON4:
-            strcat_s(str[mode], sizeof(str[mode]), "4");
+            lstrcat(str[mode], TEXT("4"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON5:
-            strcat_s(str[mode], sizeof(str[mode]), "5");
+            lstrcat(str[mode], TEXT("5"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON6:
-            strcat_s(str[mode], sizeof(str[mode]), "6");
+            lstrcat(str[mode], TEXT("6"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON7:
-            strcat_s(str[mode], sizeof(str[mode]), "7");
+            lstrcat(str[mode], TEXT("7"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON8:
-            strcat_s(str[mode], sizeof(str[mode]), "8");
+            lstrcat(str[mode], TEXT("8"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON9:
-            strcat_s(str[mode], sizeof(str[mode]), "9");
+            lstrcat(str[mode], TEXT("9"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON11:
-            strcat_s(str[mode], sizeof(str[mode]), "0");
+            lstrcat(str[mode], TEXT("0"));
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON10:
             mode = 1;
             op = plus;
+            Edit_SetText(hEWnd, TEXT("+"));
             return TRUE;
         case IDC_BUTTON13:
             mode = 1;
             op = minus;
+            Edit_SetText(hEWnd, TEXT("-"));
             return TRUE;
         case IDC_BUTTON14:
             mode = 1;
             op = kake;
+            Edit_SetText(hEWnd, TEXT("x"));
             return TRUE;
         case IDC_BUTTON15:
             mode = 1;
             op = wari;
+            Edit_SetText(hEWnd, TEXT("ÅÄ"));
             return TRUE;
         case IDC_BUTTON19:
-            strcpy_s(tmp, sizeof(tmp), "-");
-            strcpy_s(tmp, sizeof(tmp), str[mode]);
-            strcpy_s(str[mode], sizeof(str[mode]), tmp);
+            lstrcpy(tmp,  TEXT("-"));
+            lstrcpy(tmp,  str[mode]);
+            lstrcpy(str[mode], tmp);
             Edit_SetText(hEWnd, str[mode]);
             return TRUE;
         case IDC_BUTTON16:
             switch (op) {
             case plus:
-                ans = atof(str[0]) + atof(str[1]);
+                ans = _tstof(str[0]) + _tstof(str[1]);
                 break;
             case minus:
-                ans = atof(str[0]) - atof(str[1]);
+                ans = _tstof(str[0]) - _tstof(str[1]);
                 break;
             case kake:
-                ans = atof(str[0]) * atof(str[1]);
+                ans = _tstof(str[0]) * _tstof(str[1]);
                 break;
             case wari:
-                ans = atof(str[0]) / atof(str[1]);
+                ans = _tstof(str[0]) / _tstof(str[1]);
                 break;
             }
-            sprintf_s(ans_str, sizeof(ans_str), "%.10f", ans);
+            _stprintf_s(ans_str, sizeof(ans_str), TEXT("%.10f"), ans);
             Edit_SetText(hEWnd, ans_str);
             memset(str, '\0', sizeof(str));
             mode = 0;
